762920266932fa2a08fba6ab933f6a0ef5545920
chinns3527 <sparksai555@gmail.com>
2024-10-15 19:17:01 +0530

    added all files

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from factorial import factorial
 from fibonacci import fibonacci
+from temp import func1
+from temp import func2
+import threading
+from file_read import read_input_from_file
+import os
 
 def main():
     n=int(input("enter a value"))
     print("Factorial of "+str(n)+":", factorial(n))
     print("First "+str(n)+" Fibonacci numbers:", fibonacci(n))
 
+    t1 = threading.Thread(target=func1)
+    t2 = threading.Thread(target=func2)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+    print("ossssssssssssssssssssssssssssssssssssssssssssssssssssssssss",os.curdir)
+    input_file_path = os.path.join(os.curdir, 'sub_folder', 'inp.txt')
+    # Read inputs from the file
+    inputs = read_input_from_file(input_file_path)
+    print(inputs)
+
+
 if __name__ == "__main__":
     main()
+
