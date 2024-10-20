cae23ce21cac4e22416c066cd0c1c8dc003054bd
satya-adavi <satya.adavi35@gmail.com>
2024-10-20 15:45:05 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,24 @@
-def fibonacci(n):
-    if n <= 0:
-        return []
-    elif n == 1:
-        return [0]
-    elif n == 2:
-        return [0, 1]
+# def fibonacci(n):
+#     if n <= 0:
+#         return []
+#     elif n == 1:
+#         return [0]
+#     elif n == 2:
+#         return [0, 1]
+#     else:
+#         fib = [0, 1]
+#         for i in range(2, n):
+#             fib.append(fib[i - 1] + fib[i - 2])
+#         return fib
+def fibo(a,b,start,end):
+    c=a+b
+    a=b
+    b=c
+
+    print(start)
+    if start<end:
+        start = start + 1
+        return fibo(a,b,start,end)
     else:
-        fib = [0, 1]
-        for i in range(2, n):
-            fib.append(fib[i - 1] + fib[i - 2])
-        return fib
+        return c
+# print(fibo(0,1,0,5))
