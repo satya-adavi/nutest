6aded78434f146c131db300cc4fede0781c0dce6
satya-adavi <satya.adavi35@gmail.com>
2024-10-24 14:33:40 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -10,15 +10,19 @@
 #         for i in range(2, n):
 #             fib.append(fib[i - 1] + fib[i - 2])
 #         return fib
-def fibo(a,b,start,end):
-    c=a+b
-    a=b
-    b=c
 
-    print(start)
-    if start<end:
+# # print(fibo(0,1,0,5))
+def fibo(a, b, start, end):
+    if a==0:
+        print(a)
+        print(b)
+    c = a + b
+
+    print(c)
+    if start < end:
+        a, b = b, c
         start = start + 1
-        return fibo(a,b,start,end)
+        return fibo(a, b,start,end)
     else:
         return c
-# print(fibo(0,1,0,5))
+fibo(0,1,1,6)
